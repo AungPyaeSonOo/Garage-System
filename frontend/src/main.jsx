082c32127dc07a4877158233bfdc4718fafa202d
unknown <aungpyaesonoo002@gmail.com>
2026-05-02 17:55:15 +0630

    second fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import './styles/App.css'
-import './mobile-fixes.css';
+import './styles/mobile-fixes.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
